bea3db1ce578c2cca0bee45f93daaeeea1528a1a
greenmat007 <ggreenmat@gmail.com>
2024-11-10 23:11:58 +0800

    modified

--- app.py
+++ app.py
@@ -376,5 +376,5 @@ def check_gemini():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates database tables if they don't exist
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
